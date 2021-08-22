8a22affea18bc904164fec5f70aeea043c439e03
quangvinhv242 <85137558+quangvinhv242@users.noreply.github.com>
2021-08-22 21:14:30 +0700

    Add files via upload

---
+++ slide.js
@@ -0,0 +1,15 @@
+function faceBok() {
+    location.replace("https://www.facebook.com/q.vinh.104");
+}
+
+function staGam() {
+    location.replace("/storage/emulated/0/Pictures/nametag.png");
+}
+
+function twiste() {
+    location.replace("https://twitter.com/nguynvi93274299?s=09");
+}
+
+function tikTok() {
+    location.replace("https://vt.tiktok.com/ZSJ7qHqmS/");
+}
